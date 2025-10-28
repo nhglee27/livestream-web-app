d461e3a31ec2423e0f31707425c4daf68fdc5abf
DOXUANHAU <doxuanhau08022003@gmail.com>
2025-10-28 12:54:54 +0700

    add tailwind css and implement login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,11 +5,9 @@ import './styles';
 export default function App() {
   return (
     <>
-      <Header />
       <main className="min-h-screen">
         <AppRoutes />
       </main>
-      <Footer />
     </>
   );
 }
--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar: React.FC = () => {
+const Header: React.FC = () => {
   return (
     <nav className="w-full bg-white shadow-sm p-4 flex justify-between items-center">
-      <h2 className="text-xl font-semibold">MyLogo</h2>
       <div className="flex space-x-4">
         <h1>this is header</h1>
       </div>
@@ -12,4 +11,4 @@ const Navbar: React.FC = () => {
   );
 };
 
-export default Navbar;
+export default Header;
